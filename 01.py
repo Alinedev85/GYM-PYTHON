af3c1811baceb197e45a338a5414ec692439a387
Aline A. Ferreira <likaaleksiejuk85@gmail.com>
2023-11-30 14:10:58 -0300

    01

---
+++ 01.py
@@ -0,0 +1,12 @@
+# nome = input("Digite o seu nome: ")
+# print(nome) 
+
+
+
+#2. Faça um algoritmo que pergunte ao usuário quantos anos ele tem, depois disso, escreva True no console, caso ele já tenha alcançado a maioridade (18 anos), caso contrário escreva False.
+
+# idade = int(input("digite a sua idade: "))
+
+# maioridade = idade >=18
+
+# print(maioridade)
